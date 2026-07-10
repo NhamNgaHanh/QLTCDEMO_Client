9962253f545f4bbead85c1b1b001d8c504261442
Nham Nga Hanh <odieler@gmail.com>
2026-07-10 10:08:48 +0700

    Khởi tạo dự án QLTC

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import { HashRouter } from "react-router-dom";
 
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-</HashRouter>
-);
+  </StrictMode>,
+)
