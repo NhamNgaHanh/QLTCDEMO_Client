95b312877d544a619994dfa8a0ee82f682e3aa18
Nham Nga Hanh <odieler@gmail.com>
2026-07-10 08:58:57 +0700

    Khởi tạo dự án QLTC

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+</HashRouter>
 );
